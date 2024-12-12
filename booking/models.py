42728aa6258540299a33cccfb6c1ab3f46d355a7
Alex <alexander.tastad@hotmail.com>
2024-12-12 15:35:30 +0100

    Add: models for Barber, Customer, and Booking

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Barber(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    barber = models.ForeignKey(Barber, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    service = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.customer.name} - {self.date} at {self.time}'
