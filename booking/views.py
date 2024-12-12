154bc06cf2ff3f2266f16b1949cebb860d6685f4
Alex <alexander.tastad@hotmail.com>
2024-12-12 19:12:39 +0100

    Add: booking form and functionality

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Booking
+from .forms import BookingForm
 
 def home(request):
     bookings = Booking.objects.all()
     return render(request, 'booking/home.html', {'bookings': bookings})
+
+
+def book_appointment(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = BookingForm()
+    return render(request, 'booking/booking_form.html', {'form': form})
